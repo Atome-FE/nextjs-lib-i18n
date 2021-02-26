34d83671c7aa3f82b832ceaebf5f247616a70d0e
hlhr202 <hlhr202@163.com>
2021-02-26 14:41:41 +0800

    try resolve link issue

--- src/link.tsx
+++ src/link.tsx
@@ -5,17 +5,21 @@ import type { LinkProps } from 'next/link';
 
 export const Link: React.FC<LinkProps> = ({
   children,
-  href,
+  href: originHref,
   as: originAs,
   ...rest
 }) => {
   const localeContext = React.useContext(LocaleContext);
   const i18n = localeContext.i18n;
   const currentLanguage = i18n.useCurrentLanguage();
+  const href =
+    currentLanguage === i18n.deafultLocale
+      ? originHref
+      : `/${currentLanguage}${originHref}`;
   const as =
     currentLanguage === i18n.deafultLocale
-      ? originAs ?? href
-      : `/${currentLanguage}${originAs ?? href}`;
+      ? originAs ?? originHref
+      : `/${currentLanguage}${originAs ?? originHref}`;
   return (
     <_Link href={href} as={as} {...rest}>
       {children}
